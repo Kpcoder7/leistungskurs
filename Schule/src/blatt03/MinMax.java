414852011371c9f9c016759351a1e04e78f186b5
KarolP-tech <karol.paschek@ferienwohnung-zornheim.de>
2025-09-02 15:20:40 +0200

    2.09 Bugfixes der Arbeit an Aufgabe 1, Blatt 3

--- Schule/src/blatt03/MinMax.java
+++ Schule/src/blatt03/MinMax.java
@@ -6,6 +6,7 @@ public class MinMax {
     public static void main(String[] args) {
         Scanner scannerName = new Scanner(System.in);
 
+        //Eingabe der Zahlen
         System.out.println("Nenne mir eine (ganze) Zahl");
         int ersteZahl = scannerName.nextInt();
 
@@ -16,22 +17,43 @@ public class MinMax {
         int dritteZahl = scannerName.nextInt();
 
 
-        if (ersteZahl > zweiteZahl) {
-            if (ersteZahl > dritteZahl) {
+        //Berechnung größte Zahl
+
+        if (ersteZahl >= zweiteZahl) {
+            if (ersteZahl >= dritteZahl) {
                 System.out.println("Die größte Zahl lautet " + ersteZahl);
             }
             else {
                 System.out.println("Die größte Zahl lautet " + dritteZahl);
             }
         }
-        else if (ersteZahl < zweiteZahl) {
-            if (zweiteZahl > dritteZahl) {
+        else if (ersteZahl <= zweiteZahl) {
+            if (zweiteZahl >= dritteZahl) {
                 System.out.println("Die größte Zahl lautet " + zweiteZahl);
             }
             else {
                 System.out.println("Die größte Zahl lautet " + dritteZahl);
             }
-        } //TODO: Fall verarbeiten, wenn zwei Zahlen gleich groß sind
-        //TODO: Kleinste Zahl mit ausgeben
+        }
+
+        //Berechnung kleinste Zahl
+        if (ersteZahl <= zweiteZahl) {
+            if (ersteZahl <= dritteZahl) {
+                System.out.println("Die kleinste Zahl lautet " + ersteZahl);
+            }
+            else {
+                System.out.println("Die kleinste Zahl lautet " + dritteZahl);
+            }
+        }
+        else if (ersteZahl >= zweiteZahl) {
+            if (zweiteZahl <= dritteZahl) {
+                System.out.println("Die kleinste Zahl lautet " + zweiteZahl);
+            }
+            else {
+                System.out.println("Die kleinste Zahl lautet " + dritteZahl);
+            }
+        }
+
+        //TODO: Fall verarbeiten, wenn zwei Zahlen gleich groß sind
     }
 }
